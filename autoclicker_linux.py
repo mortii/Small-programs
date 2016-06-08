46ee941c3ff5569ffa096a05baa723b49a8ff7df
mortii <mortteenn@gmail.com>
2016-06-08 19:23:23 +0200

    added spacing

--- autoclicker_linux.py
+++ autoclicker_linux.py
@@ -4,9 +4,11 @@ from Tkinter import *
 import tkMessageBox
 import re
 
+
 def main():
 	auto_clicker = Application(None)
 	auto_clicker.mainloop()
+
 
 class Application(Tk):
 	def __init__(self, parent):
@@ -21,16 +23,19 @@ class Application(Tk):
 		self.bind_keys_and_widgets()
 		self.pack_to_window()
 
+
 	def initialize_window(self):
 		self.title("Autoclicker")
 		self.geometry("370x265")
 		self.configure(bg=self.bg_color)
+
 
 	def initialize_variables(self):
 		self.click_delay = None
 		self.saved_x = False;
 		self.saved_y = False
 		self.mouse = PyMouse()
+
 
 	def initialize_text(self):
 		self.click_delay_txt_prev = StringVar()
@@ -50,11 +55,13 @@ class Application(Tk):
 		self.start_clicker_text.set("\nTo start press the 'a' key or the button."
 			"\nTo stop just move your mouse.\n")
 
+
 	def update_cps(self, *trace_args):
 		if self.valid_input():
 			self.click_delay = float(self.click_delay_txt_curr.get())
 			cps = 1.0 / self.click_delay
 			self.cps_text.set("Clicks per second: %.3f" % cps)
+
 	
 	def valid_input(self):
 		current_delay = self.click_delay_txt_curr.get()
@@ -73,6 +80,7 @@ class Application(Tk):
 		else:
 			self.click_delay_txt_curr.set(prev_delay)
 			return False
+
 	
 	def initialize_labels(self):
 		self.button = Button(self, text="Start Autoclicker", bg="blue", fg="white")
@@ -80,20 +88,24 @@ class Application(Tk):
 		self.spinbox = Spinbox(self, format="%.3f", increment=0.005, from_=0.01, to=100.0)
 		self.spinbox.configure(width=10, textvariable=self.click_delay_txt_curr)
 
+
 	def initialize_widgets(self):
 		self.info_label = Label(self, text=self.info_text, bg=self.bg_color)
 		self.cursor_pos_label = Label(self, textvariable=self.cursor_pos_text, bg=self.bg_color, foreground="red")
 		self.cps_label = Label(self, textvariable=self.cps_text, bg=self.bg_color, foreground="red")
 		self.start_clicker_label = Label(self, textvariable=self.start_clicker_text, bg=self.bg_color)
+
 	
 	def bind_keys_and_widgets(self):
 		self.bind("c", self.set_cursor_position)
 		self.bind("a", self.click_mouse)	
 		self.button.bind("<Button-1>", self.click_mouse)
 
+
 	def set_cursor_position(self, event):
 		self.saved_x, self.saved_y = self.mouse.position() 
 		self.cursor_pos_text.set("\nSaved cursor position: %dx, %dy" % (self.saved_x, self.saved_y)) 
+
 
 	def click_mouse(self, event):
 		if not self.saved_x  and not self.saved_y:
@@ -114,6 +126,7 @@ class Application(Tk):
 
 		print "finished, moved mouse"
 
+
 	def pack_to_window(self):
 		self.info_label.pack()
 		self.spinbox.pack()
@@ -122,6 +135,7 @@ class Application(Tk):
 		self.start_clicker_label.pack()
 		self.button.pack()
 		self.resizable(False, False)
+
 
 if __name__ == '__main__':
 	main()
